d5e5028531a4ce06e36e5931de7bdcf909ef5bf0
Federico Villace <federicovillace@gmail.com>
2023-02-13 20:51:33 -0300

    clean up code

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,30 +1,7 @@
-import noResults from "../mocks/no-results.json";
 import { useState } from "react";
 
 const URL = "https://www.omdbapi.com/";
-const API_KEY = "f425c4f9";
-
-export async function setSearch({ search }) {
-  if (search === "") return null;
-
-  try {
-    const response = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
-    );
-    const json = await response.json();
-
-    const movies = json.Search;
-
-    return movies?.map((movie) => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      image: movie.Poster,
-    }));
-  } catch (e) {
-    throw new Error("Error searching movies");
-  }
-}
+import { setSearch } from "../services/movies";
 
 export function useMovies({ search }) {
   const [movies, setMovies] = useState([]);
---
+++ src/utils/keys.js
@@ -0,0 +1,1 @@
+export const API_KEY = "f425c4f9";
